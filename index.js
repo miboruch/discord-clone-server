d7e18594438fcc6fab78b718ab2c9a82f6390469
Michał Boruch <miboruch@gmail.com>
2020-04-16 19:29:01 +0200

    Moved namespace socket listeners to another file

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const Namespace = require('./models/Namespace');
 const namespaceModule = require('./modules/namespacesModule');
 require('dotenv').config();
 const mainConnectionEvents = require('./socket/mainConnectionEvents');
+const namespaceConnectionEvent = require('./socket/namespaceConnectionEvents');
 const helperModule = require('./utils/helpers');
 
 const userRoutes = require('./routes/userRoutes');
@@ -28,8 +29,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
-const usersOnline = [];
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
@@ -51,13 +50,6 @@ connection.once('open', async () => {
     await mainConnectionEvents(socket);
   });
 
-  const rooms = await roomController.getAllRooms();
-  const allRooms = rooms.map(room => {
-    return { id: room._id.toString(), users: [] };
-  });
-
-  const namespaceUsers = [];
-
   const namespaceEventEmitter = Namespace.watch();
 
   namespaceEventEmitter.on('change', async change => {
@@ -72,190 +64,13 @@ connection.once('open', async () => {
    * will be executed
    */
   namespaceModule.subscribe(value => {
-    value.map(item => {
-      io.of(`/${item._id}`)
+    value.map(namespace => {
+      io.of(`/${namespace._id}`)
         .use((namespaceSocket, next) => {
           socketAuthentication(namespaceSocket, next);
         })
         .on('connection', async namespaceSocket => {
-          console.log(namespaceSocket.decoded);
-          const currentNamespace = io.of(`/${item._id}`);
-
-          /* store in online users array */
-          if (
-            namespaceUsers.filter(
-              user => user.userID === namespaceSocket.decoded._id
-            ).length === 0
-          ) {
-            namespaceUsers.push({
-              socketID: namespaceSocket.id,
-              userID: namespaceSocket.decoded._id
-            });
-          }
-
-          namespaceSocket.emit('namespace_joined', item);
-          console.log(`joined namespace ${item._id}`);
-
-          /* load rooms */
-          namespaceSocket.emit(
-            'load_rooms',
-            await roomController.getAllNamespaceRooms(item._id)
-          );
-
-          /* get namespace data */
-          namespaceSocket.emit(
-            'namespace_data',
-            await namespaceController.getNamespaceData(item._id)
-          );
-
-          /* create room */
-          namespaceSocket.on('create_room', async ({ name, description }) => {
-            console.log('CREATE ROOM');
-            const savedRoom = await roomController.createNewRoom(
-              name,
-              description,
-              item._id
-            );
-
-            allRooms.push({
-              id: savedRoom._id.toString(),
-              users: [namespaceSocket.decoded._id]
-            });
-
-            const namespaceRooms = await roomController.getAllNamespaceRooms(
-              item._id
-            );
-
-            /* emit to everyone in the namespace */
-            currentNamespace.emit('room_created', namespaceRooms);
-            namespaceSocket.join(savedRoom._id);
-            namespaceSocket.emit('user_joined', savedRoom._id.toString());
-          });
-
-          /* LEAVE ROOM */
-          namespaceSocket.on('leave_room', roomName => {
-            console.log('LEAVE ROOM EVENT');
-            namespaceSocket.leave(roomName, () => {
-              currentNamespace.in(roomName).clients((error, clients) => {
-                currentNamespace
-                  .in(roomName)
-                  .emit('members_update', clients.length);
-              });
-            });
-          });
-
-          /* JOIN ROOM */
-          namespaceSocket.on('join_room', async ({ roomName, roomID }) => {
-            try {
-              const [currentRoomInfo] = await roomController.getSingleRoomInfo(
-                roomID
-              );
-
-              namespaceSocket.join(roomName, () => {
-                currentNamespace.in(roomName).clients((error, clients) => {
-                  console.log(clients);
-                  currentNamespace
-                    .in(roomName)
-                    .emit('members_update', clients.length);
-                });
-              });
-
-              namespaceSocket.emit('user_joined', {
-                roomName: roomName,
-                roomInfo: currentRoomInfo
-              });
-
-              namespaceSocket.emit(
-                'history_catchup',
-                await messageController.fetchHistoryMessages(roomName)
-              );
-            } catch (error) {
-              namespaceSocket.emit('error', 'Error with joining to the room');
-            }
-          });
-
-          /* USER IS TYPING */
-          namespaceSocket.on(
-            'user_typing',
-            ({ room, userID, name, lastName }) => {
-              namespaceSocket.broadcast
-                .to(room)
-                .emit('user_is_typing', { userID, name, lastName });
-            }
-          );
-
-          /* USER STOPPED TYPING */
-          namespaceSocket.on(
-            'user_finished_typing',
-            ({ room, userID, name, lastName }) => {
-              namespaceSocket.broadcast
-                .to(room)
-                .emit('user_is_not_typing', { userID, name, lastName });
-            }
-          );
-
-          /* SEND RECEIVED MESSAGE */
-          namespaceSocket.on(
-            'send_message',
-            async ({ message, room, userName, userID }) => {
-              const savedMessage = await messageController.saveMessage(
-                message,
-                room,
-                userName.name,
-                userName.lastName,
-                userID
-              );
-
-              currentNamespace.to(room).emit('new_message', savedMessage);
-            }
-          );
-
-          /* DELETE NAMESPACE */
-          namespaceSocket.on('delete_namespace', async ({ namespaceID }) => {
-            currentNamespace.emit(
-              'leave_namespace',
-              'Namespace has been deleted'
-            );
-            await namespaceController.removeNamespace(namespaceID);
-            namespaceSocket.emit('information', {
-              type: 'success',
-              message: 'Namespace has been deleted'
-            });
-          });
-
-          /* refresh namespaces */
-          namespaceSocket.on('reload_namespaces', async ({ userID }) => {
-            namespaceSocket.emit(
-              'namespaces_reloaded',
-              await namespaceController.getAllUserNamespaces(userID)
-            );
-          });
-
-          namespaceSocket.on('delete_room', async ({ roomID, roomName }) => {
-            try {
-              const room = `${roomID}${slugify(roomName)}`;
-
-              currentNamespace.in(room).emit('leave_room', 'You have left this room');
-
-              currentNamespace.in(room).clients((error, clients) => {
-                console.log(clients);
-                clients.map(item => {
-                  currentNamespace.connected[item].leave(room);
-                });
-              });
-
-              await roomController.removeSingleRoom(roomID, roomName);
-            } catch (error) {
-              console.log(error);
-            }
-          });
-
-          namespaceSocket.on('namespace_disconnect', () => {
-            usersOnline.filter(
-              user => user.userID !== namespaceSocket.decoded._id
-            );
-            // namespaceSocket.disconnect();
-          });
+          await namespaceConnectionEvent(namespaceSocket, namespace)
         });
     });
   });
